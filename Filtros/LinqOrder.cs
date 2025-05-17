032831f98fe716df75ffd71947e8faef198d3fc2
Lucas Bailo <lucasebailo@gmail.com>
2025-05-17 17:24:16 -0300

    first commit

---
+++ Filtros/LinqOrder.cs
@@ -0,0 +1,17 @@
+﻿using NewScreenSound.Modelos;
+
+namespace NewScreenSound.Filtros;
+
+internal class LinqOrder
+{
+    public static void ExibirListaDeArtistasOrdenados(List<Musica> musicas) 
+    {
+        var artistasOrdenados = musicas.OrderBy(x => x.Artista).Select(x => x.Artista).Distinct().ToList();
+        Console.WriteLine("Lista de Artistas Ordenados: \n");
+        foreach (var artista in artistasOrdenados)
+        {
+            Console.WriteLine($"- {artista}");
+        }
+    }
+}
+
